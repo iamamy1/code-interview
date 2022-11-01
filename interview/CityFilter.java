b0dbad8b11a4ac4e4d16343010bf4fe207949bd1
ameliaspace <90300974+iamamy1@users.noreply.github.com>
2022-11-01 15:21:28 +0800

    Update CityFilter.java

--- interview/CityFilter.java
+++ interview/CityFilter.java
@@ -10,7 +10,7 @@ public class CityFilter {
      * 现有的城市有以下情况：
      * "1,2,3,4"  表示城市1，2，3，4都在范围内，  这时 CityFilter.contains(1) -> true;  CityFilter.contains(5) -> false
      * "-1,-2"    表示除去 1, 2 两个城市外其他城市是在范围内  这时 CityFilter.contains(1) -> false;  CityFilter.contains(3) -> true
-     * "&"        表示国内城市满足  这时： CityFilter.contains(100) -> true; CityFilter.contains(10001) -> true
+     * "&"        表示国内城市满足  这时： CityFilter.contains(100) -> true; CityFilter.contains(10001) -> false
      * "$"        表示海外城市满足范围内
      * 说明： 当cityid <= 10000 为国内城市， 当cityid > 10000 海外城市
      */
@@ -22,6 +22,19 @@ public class CityFilter {
         /**
          * 实现要求：最小内存使用和CPU计算
          */
+        char first = citys.charAt(0);
+        if(first == '&'){
+            return city <= 10000 ? true : false;
+        }else if(first == '$'){
+            return city <= 10000 ? false : true;
+        }else if(first == '-'){
+            city = -city;
+            String cityStr = String.valueOf(city);
+            return !Arrays.asList(citys.split(",")).contains(cityStr);
+        }else{
+            String cityStr = String.valueOf(city);
+            return Arrays.asList(citys.split(",")).contains(cityStr);
+        }
         return true;
     }
 }
